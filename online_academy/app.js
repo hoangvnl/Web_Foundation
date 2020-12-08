4c60bfdc7e83c4dc6de73d71fb0e1a1b065b2549
hoangvnl <73113401+hoangvnl@users.noreply.github.com>
2020-12-08 19:58:23 +0700

    404 error

--- online_academy/app.js
+++ online_academy/app.js
@@ -3,12 +3,23 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({
+    defaultLayout: 'main.hbs'
+}));
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
     res.render('home');
 });
+
+app.use(function (req, res) {
+    res.render('404');
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
